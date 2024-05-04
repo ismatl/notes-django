ee720961302c1b22ec8d09f86c6900ecdc1c0807
ismatl <168882718+ismatl@users.noreply.github.com>
2024-05-04 16:46:16 -0500

    fix: update project_apps in settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -30,15 +30,20 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INTERNAL_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+]
+
+PROJECT_APPS = [
     'tasks'
 ]
+
+INSTALLED_APPS = INTERNAL_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
